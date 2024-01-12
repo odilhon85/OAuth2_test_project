5134b420d96ef2f4e9a03abc730bb5cc14d6f922
odilhon85 <i.o.r@inbox.ru>
2024-01-12 16:34:48 +0500

    Initial commit

---
+++ src/main/java/uz/devior/oauth2testproject/OAuth2TestProjectApplication.java
@@ -0,0 +1,13 @@
+package uz.devior.oauth2testproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2TestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2TestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/devior/oauth2testproject/OAuth2TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.devior.oauth2testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2TestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
